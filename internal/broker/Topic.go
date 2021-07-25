5f97f2774682a87d22e22becdb8cc178b9a4bc81
sinamna <sina3050@gmail.com>
2021-07-25 16:48:06 +0430

    renamed file - split models

--- internal/broker/Topic.go
+++ internal/broker/Topic.go
@@ -2,6 +2,7 @@ package broker
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"therealbroker/internal/utils"
 	"therealbroker/pkg/broker"
@@ -12,26 +13,53 @@ var MessageID = AutoIncId{id: 1}
 type Subscriber struct {
 	sync.RWMutex
 	Channel chan broker.Message
+	ready chan struct{}
 	Ctx     context.Context
 	Buffer []broker.Message
 	BufferIndex int
 }
 
-func (s *Subscriber) publishMessage(msg broker.Message) {
-	select {
-	case <-s.Ctx.Done():
-	default:
-		s.Channel <- msg
+func (s *Subscriber) registerMessage(msg broker.Message){
+	s.Lock()
+	defer s.Unlock()
+	s.BufferIndex++
+	if s.BufferIndex <20 {
+			s.Buffer[s.BufferIndex]=msg
+	}else{
+		s.Buffer = append(s.Buffer,msg)
 	}
 }
+func (s *Subscriber) publishMessage(){
+	for{
+		select {
+		case <-s.Ctx.Done():
+			return
+		case <-s.ready:
+			fmt.Println("got ready")
+			s.Lock()
+			msgs := s.Buffer[:s.BufferIndex]
+			s.BufferIndex=-1
+			s.Unlock()
+			go func(){
+				for _,msg := range msgs{
+					s.Channel<-msg
+				}
+			}()
+		default:
+			fmt.Println("default")
+		}
+	}
 
+}
 func CreateNewSubscriber(ctx context.Context, ch chan broker.Message)*Subscriber{
 	newSub:= &Subscriber{
 		Channel: ch,
+		ready: make(chan struct{}),
 		Ctx: ctx,
 		Buffer: make([]broker.Message,20),
-		BufferIndex: 0,
+		BufferIndex: -1,
 	}
+	go newSub.publishMessage()
 	return newSub
 }
 
@@ -62,8 +90,10 @@ func (t *Topic) registerSub() {
 
 func (t *Topic) PublishMessage(msg broker.Message) int {
 	for _, sub := range t.Subscribers {
-		go sub.publishMessage(msg)
-
+		sub.registerMessage(msg)
+		sub := sub
+		go func(){sub.ready<- struct{}{}
+		}()
 		//TODO: Can we add concurrency here?
 	}
 	messageId := MessageID.GetID()
