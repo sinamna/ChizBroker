a986426b01a81c0ddda6a43a1d1c9a05d512141f
sinamna <sina3050@gmail.com>
2021-07-25 14:08:40 +0430

    added buffered Subscriber

--- internal/broker/model.go
+++ internal/broker/model.go
@@ -2,13 +2,19 @@ package broker
 
 import (
 	"context"
+	"sync"
 	"therealbroker/internal/utils"
 	"therealbroker/pkg/broker"
 )
+
 var MessageID = AutoIncId{id: 1}
+
 type Subscriber struct {
+	sync.RWMutex
 	Channel chan broker.Message
 	Ctx     context.Context
+	Buffer []broker.Message
+	BufferIndex int
 }
 
 func (s *Subscriber) publishMessage(msg broker.Message) {
@@ -19,69 +25,75 @@ func (s *Subscriber) publishMessage(msg broker.Message) {
 	}
 }
 
-
+func CreateNewSubscriber(ctx context.Context, ch chan broker.Message)*Subscriber{
+	newSub:= &Subscriber{
+		Channel: ch,
+		Ctx: ctx,
+		Buffer: make([]broker.Message,20),
+		BufferIndex: 0,
+	}
+	return newSub
+}
 
 
 type Topic struct {
 	Name        string
 	Subscribers []*Subscriber
 	Messages    map[int]broker.Message
-	IDs map[int]struct{}
-	subChannel chan *Subscriber
+	IDs         map[int]struct{}
+	subChannel  chan *Subscriber
 }
 
-func (t *Topic) RegisterSubscriber(ctx context.Context)chan broker.Message{
+func (t *Topic) RegisterSubscriber(ctx context.Context) chan broker.Message {
 	ch := make(chan broker.Message)
-	newSub := &Subscriber{
-		Channel: ch,
-		Ctx:     ctx,
-	}
+	newSub := CreateNewSubscriber(ctx,ch)
 	t.subChannel <- newSub
 	return ch
 }
 
-func (t *Topic) registerSub(){
+func (t *Topic) registerSub() {
 	for {
-		select{
+		select {
 		case sub := <-t.subChannel:
 			t.Subscribers = append(t.Subscribers, sub)
 		}
 	}
 }
-func (t *Topic) PublishMessage(msg broker.Message)int{
+
+func (t *Topic) PublishMessage(msg broker.Message) int {
 	for _, sub := range t.Subscribers {
 		go sub.publishMessage(msg)
 
 		//TODO: Can we add concurrency here?
 	}
 	messageId := MessageID.GetID()
-	t.IDs[messageId]=struct{}{}
+	t.IDs[messageId] = struct{}{}
 
-	if msg.Expiration != 0{
+	if msg.Expiration != 0 {
 		t.Messages[messageId] = msg
-		go utils.WatchForExpiration(t.Messages,messageId,msg.Expiration)
+		go utils.WatchForExpiration(t.Messages, messageId, msg.Expiration)
 	}
 	return messageId
 }
 
 func NewTopic(name string) *Topic {
 	subscribers := make([]*Subscriber, 0)
-	newTopic :=&Topic{
+	newTopic := &Topic{
 		Name:        name,
 		Subscribers: subscribers,
 		Messages:    map[int]broker.Message{},
-		IDs: map[int]struct{}{},
-		subChannel: make(chan *Subscriber),
+		IDs:         map[int]struct{}{},
+		subChannel:  make(chan *Subscriber),
 	}
 	go newTopic.registerSub()
 	return newTopic
 }
 
-
-type AutoIncId struct{
+type AutoIncId struct {
 	id int
 }
-func (ai *AutoIncId) GetID()(id int){
+
+func (ai *AutoIncId) GetID() (id int) {
 	id = ai.id
 	ai.id++
 	return
