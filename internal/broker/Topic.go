15ad2180bf8fc0426615ccd06b5cdabc92a3aa4b
sinamna <sina3050@gmail.com>
2021-07-25 16:48:32 +0430

    fixed adding subscribers bug

--- internal/broker/Topic.go
+++ internal/broker/Topic.go
@@ -2,7 +2,6 @@ package broker
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"therealbroker/internal/utils"
 	"therealbroker/pkg/broker"
@@ -10,90 +9,36 @@ import (
 
 var MessageID = AutoIncId{id: 1}
 
-type Subscriber struct {
-	sync.RWMutex
-	Channel chan broker.Message
-	ready chan struct{}
-	Ctx     context.Context
-	Buffer []broker.Message
-	BufferIndex int
-}
-
-func (s *Subscriber) registerMessage(msg broker.Message){
-	s.Lock()
-	defer s.Unlock()
-	s.BufferIndex++
-	if s.BufferIndex <20 {
-			s.Buffer[s.BufferIndex]=msg
-	}else{
-		s.Buffer = append(s.Buffer,msg)
-	}
-}
-func (s *Subscriber) publishMessage(){
-	for{
-		select {
-		case <-s.Ctx.Done():
-			return
-		case <-s.ready:
-			fmt.Println("got ready")
-			s.Lock()
-			msgs := s.Buffer[:s.BufferIndex]
-			s.BufferIndex=-1
-			s.Unlock()
-			go func(){
-				for _,msg := range msgs{
-					s.Channel<-msg
-				}
-			}()
-		default:
-			fmt.Println("default")
-		}
-	}
-
-}
-func CreateNewSubscriber(ctx context.Context, ch chan broker.Message)*Subscriber{
-	newSub:= &Subscriber{
-		Channel: ch,
-		ready: make(chan struct{}),
-		Ctx: ctx,
-		Buffer: make([]broker.Message,20),
-		BufferIndex: -1,
-	}
-	go newSub.publishMessage()
-	return newSub
-}
-
 
 type Topic struct {
+	sync.Mutex
 	Name        string
 	Subscribers []*Subscriber
 	Messages    map[int]broker.Message
 	IDs         map[int]struct{}
-	subChannel  chan *Subscriber
 }
 
 func (t *Topic) RegisterSubscriber(ctx context.Context) chan broker.Message {
 	ch := make(chan broker.Message)
 	newSub := CreateNewSubscriber(ctx,ch)
-	t.subChannel <- newSub
+	t.Subscribers = append(t.Subscribers, newSub)
 	return ch
 }
 
-func (t *Topic) registerSub() {
-	for {
-		select {
-		case sub := <-t.subChannel:
-			t.Subscribers = append(t.Subscribers, sub)
-		}
-	}
-}
+//func (t *Topic) registerSub() {
+//	for {
+//		select {
+//		case sub := <-t.subChannel:
+//			t.Lock()
+//
+//			t.Unlock()
+//		}
+//	}
+//}
 
 func (t *Topic) PublishMessage(msg broker.Message) int {
 	for _, sub := range t.Subscribers {
 		sub.registerMessage(msg)
-		sub := sub
-		go func(){sub.ready<- struct{}{}
-		}()
 		//TODO: Can we add concurrency here?
 	}
 	messageId := MessageID.GetID()
@@ -113,9 +58,9 @@ func NewTopic(name string) *Topic {
 		Subscribers: subscribers,
 		Messages:    map[int]broker.Message{},
 		IDs:         map[int]struct{}{},
-		subChannel:  make(chan *Subscriber),
+
 	}
-	go newTopic.registerSub()
+	//go newTopic.registerSub()
 	return newTopic
 }
 
