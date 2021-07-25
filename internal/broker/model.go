05544f72b8cb66ee953e26ece0a00d94145e13e6
sinamna <sina3050@gmail.com>
2021-07-25 11:14:15 +0430

    added subscriber's publish

--- internal/broker/model.go
+++ internal/broker/model.go
@@ -9,7 +9,15 @@ type Subscriber struct{
 	Channel chan broker.Message
 	Ctx context.Context
 }
-
+func(s *Subscriber) publishMessage(msg broker.Message)error{
+	select{
+	case <-s.Ctx.Done():
+		return broker.ErrExpiredID
+	default:
+		s.Channel<-msg
+		return nil
+	}
+}
 type Topic struct{
 	Name string
 	Subscribers []*Subscriber
@@ -22,6 +30,9 @@ func (t *Topic) RegisterSubscriber(ctx context.Context) chan broker.Message{
 	}
 	t.Subscribers = append(t.Subscribers, newSub)
 	return newSub.Channel
+}
+func(t *Topic) PublishMessage(msg broker.Message){
+
 }
 func NewTopic(name string)*Topic{
 	subscribers := make([]Subscriber,0)
