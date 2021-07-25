81a4c808ea40e6c6fc43097fc6146e8b00a600fb
sinamna <sina3050@gmail.com>
2021-07-25 11:11:05 +0430

    added subscriber

--- internal/broker/model.go
+++ internal/broker/model.go
@@ -1,18 +1,33 @@
 package broker
 
-import "therealbroker/pkg/broker"
+import (
+	"context"
+	"therealbroker/pkg/broker"
+)
+
+type Subscriber struct{
+	Channel chan broker.Message
+	Ctx context.Context
+}
 
 type Topic struct{
 	Name string
-	Subscribers []chan broker.Message
+	Subscribers []*Subscriber
 	Messages map[string]broker.Message
 }
-
+func (t *Topic) RegisterSubscriber(ctx context.Context) chan broker.Message{
+	newSub := &Subscriber{
+		Channel: make(chan broker.Message),
+		Ctx: ctx,
+	}
+	t.Subscribers = append(t.Subscribers, newSub)
+	return newSub.Channel
+}
 func NewTopic(name string)*Topic{
-	channels := make([]chan broker.Message,0)
+	subscribers := make([]Subscriber,0)
 	return &Topic{
 		Name: name,
-		Subscribers: channels,
+		Subscribers: subscribers,
 		Messages: map[string]broker.Message{},
 	}
 }
