8463cb2f00985101ab0e52d851842fb29bc746d1
sinamna <sina3050@gmail.com>
2021-07-25 10:27:26 +0430

    added topics

--- internal/broker/model.go
+++ internal/broker/model.go
@@ -1,1 +1,18 @@
 package broker
+
+import "therealbroker/pkg/broker"
+
+type Topic struct{
+	Name string
+	Subscribers []chan broker.Message
+	Messages map[string]broker.Message
+}
+
+func NewTopic(name string)*Topic{
+	channels := make([]chan broker.Message,0)
+	return &Topic{
+		Name: name,
+		Subscribers: channels,
+		Messages: map[string]broker.Message{},
+	}
+}
