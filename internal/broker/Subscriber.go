3271e68a8b514cbc6be02947d558d76b41a53c1b
sinamna <sina3050@gmail.com>
2021-07-26 10:20:50 +0430

    redesigned subscriber

--- internal/broker/Subscriber.go
+++ internal/broker/Subscriber.go
@@ -9,55 +9,50 @@ import (
 type Subscriber struct {
 	sync.RWMutex
 	Channel chan broker.Message
-	ready chan struct{}
 	Ctx     context.Context
-	Buffer []broker.Message
-	BufferIndex int
+
 }
 
 func (s *Subscriber) registerMessage(msg broker.Message){
-	s.Lock()
-	s.BufferIndex++
-	if s.BufferIndex <20 {
-		s.Buffer[s.BufferIndex]=msg
-	}else{
-		s.Buffer = append(s.Buffer,msg)
-	}
-	go func(){
-		s.ready<- struct{}{}
-	}()
-	s.Unlock()
+	s.Channel<-msg
+	//s.Lock()
+	//s.BufferIndex++
+	//if s.BufferIndex <20 {
+	//	s.Buffer[s.BufferIndex]=msg
+	//}else{
+	//	s.Buffer = append(s.Buffer,msg)
+	//}
+	//go func(){
+	//	s.ready<- struct{}{}
+	//}()
+	//s.Unlock()
 
 }
-func (s *Subscriber) publishMessage(){
-	for{
-		select {
-		case <-s.Ctx.Done():
-			return
-		case <-s.ready:
-			s.Lock()
-			msgs := s.Buffer[:s.BufferIndex+1]
-			s.BufferIndex=-1
-			go func(){
-				for _,msg := range msgs{
-					s.Channel<-msg
-				}
-			}()
-			s.Unlock()
+//func (s *Subscriber) publishMessage(){
+//	for{
+//		select {
+//		case <-s.Ctx.Done():
+//			return
+//		case <-s.ready:
+//			//s.Lock()
+//			//msgs := s.Buffer[:s.BufferIndex+1]
+//			//s.BufferIndex=-1
+//			go func(){
+//				for _,msg := range s.Buffer{
+//					s.Channel<-msg
+//				}
+//			}()
+//			//s.Unlock()
+//
+//		}
+//	}
 
-		}
-	}
-
-}
+//}
 func CreateNewSubscriber(ctx context.Context, ch chan broker.Message)*Subscriber{
 	newSub:= &Subscriber{
 		Channel: ch,
-		ready: make(chan struct{}),
 		Ctx: ctx,
-		Buffer: make([]broker.Message,20),
-		BufferIndex: -1,
 	}
-	go newSub.publishMessage()
 	return newSub
 }
 
