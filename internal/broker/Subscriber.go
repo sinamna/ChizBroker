e020b635b91b1e2d187a7f58fccfb279d8bc3291
sinamna <sina3050@gmail.com>
2021-07-27 09:43:40 +0430

    refactored

--- internal/broker/Subscriber.go
+++ internal/broker/Subscriber.go
@@ -15,39 +15,7 @@ type Subscriber struct {
 
 func (s *Subscriber) registerMessage(msg broker.Message){
 	s.Channel<-msg
-	//s.Lock()
-	//s.BufferIndex++
-	//if s.BufferIndex <20 {
-	//	s.Buffer[s.BufferIndex]=msg
-	//}else{
-	//	s.Buffer = append(s.Buffer,msg)
-	//}
-	//go func(){
-	//	s.ready<- struct{}{}
-	//}()
-	//s.Unlock()
-
 }
-//func (s *Subscriber) publishMessage(){
-//	for{
-//		select {
-//		case <-s.Ctx.Done():
-//			return
-//		case <-s.ready:
-//			//s.Lock()
-//			//msgs := s.Buffer[:s.BufferIndex+1]
-//			//s.BufferIndex=-1
-//			go func(){
-//				for _,msg := range s.Buffer{
-//					s.Channel<-msg
-//				}
-//			}()
-//			//s.Unlock()
-//
-//		}
-//	}
-
-//}
 func CreateNewSubscriber(ctx context.Context, ch chan broker.Message)*Subscriber{
 	newSub:= &Subscriber{
 		Channel: ch,
