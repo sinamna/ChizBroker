8765b2685bb7b5f97f5577eabe7e0ced5e7ba69b
sinamna <sina3050@gmail.com>
2021-08-02 15:04:32 +0430

    done subscribe endpoint

--- api/server/server.go
+++ api/server/server.go
@@ -26,9 +26,16 @@ func(s *Server) Publish(ctx context.Context,publishReq *pb.PublishRequest) (*pb.
 	return response,nil
 
 }
-func(s *Server) Subscribe(*pb.SubscribeRequest, pb.Broker_SubscribeServer) error{
-
-
+func(s *Server) Subscribe(req *pb.SubscribeRequest,stream pb.Broker_SubscribeServer) error{
+	ch, err :=s.broker.Subscribe(context.Background(),req.GetSubject())
+	if err!= nil{
+		return status.Errorf(codes.Unavailable,"Broker has been closed bruh.")
+	}
+	for message := range ch{
+		messageResponse := &pb.MessageResponse{Body: []byte(message.Body)}
+		stream.Send(messageResponse)
+	}
+	return nil
 }
 func(s *Server) Fetch(context.Context, *pb.FetchRequest) (*pb.MessageResponse, error){
 
