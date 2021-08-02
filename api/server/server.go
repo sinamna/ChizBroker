3a3211715607d0258e61b1629cffccb3afca605f
sinamna <sina3050@gmail.com>
2021-08-02 14:42:39 +0430

    done publish endpoint

--- api/server/server.go
+++ api/server/server.go
@@ -2,12 +2,28 @@ package server
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	pb "therealbroker/api/proto"
+	"therealbroker/pkg/broker"
+	"time"
+
 )
-type Server struct{}
+type Server struct{
+	broker broker.Broker
+}
 
-func(s *Server) Publish(context.Context, *pb.PublishRequest) (*pb.PublishResponse, error) {
-
+func(s *Server) Publish(ctx context.Context,publishReq *pb.PublishRequest) (*pb.PublishResponse, error) {
+	message:= broker.Message{
+		Body: string(publishReq.GetBody()),
+		Expiration: time.Duration(publishReq.ExpirationSeconds),
+	}
+	messageId, err := s.broker.Publish(ctx,publishReq.GetSubject(),message)
+	if err != nil {
+		return nil, status.Errorf(codes.Unavailable,"Broker has been closed bruh.")
+	}
+	response := &pb.PublishResponse{Id: int32(messageId)}
+	return response,nil
 
 }
 func(s *Server) Subscribe(*pb.SubscribeRequest, pb.Broker_SubscribeServer) error{
