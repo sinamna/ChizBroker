4271114a18a612f13ed76cf906359317aa7b3c59
sinamna <sina3050@gmail.com>
2021-08-03 11:39:56 +0430

    added starting server

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package therealbroker
 
-import "fmt"
+import (
+
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	pb "therealbroker/api/proto"
+	"therealbroker/api/server"
+)
 
 // Main requirements:
 // 1. All tests should be passed
@@ -9,5 +16,15 @@ import "fmt"
 // 	  for every base functionality ( publish, subscribe etc. )
 
 func main() {
-	fmt.Println("Hello!")
+	lis, err := net.Listen("tcp", "localhost:80086")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterBrokerServer(grpcServer,server.Server{})
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to run server: %v\n", err)
+	}
 }
