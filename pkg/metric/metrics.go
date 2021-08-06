c4d2adfcd338fd9274761967a5df77f15646906e
sinamna <sina3050@gmail.com>
2021-08-06 09:52:48 +0430

    added metrics

--- pkg/metric/metrics.go
+++ pkg/metric/metrics.go
@@ -1,1 +1,25 @@
 package metric
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	methodDuration = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Name:        "broker_call_duration",
+		Help:        "calculating the latency of grpc calls",
+	},[]string{"method"})
+
+	activeSubscribers = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "broker_active_subscribers",
+		Help: "number of active subscribers in broker",
+	})
+	methodCalls = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "broker_active_subscribers",
+		Help: "number of active subscribers in broker",
+	},[]string{"method"})
+)
+
+func init(){
+	prometheus.Register(methodDuration)
+	prometheus.Register(activeSubscribers)
+	prometheus.Register(methodCalls)
+}
