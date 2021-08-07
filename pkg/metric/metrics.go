7755f46b9585d609ff6347e5580b24355ce16700
sinamna <sina3050@gmail.com>
2021-08-07 13:26:30 +0430

    trying to fix metrics

--- pkg/metric/metrics.go
+++ pkg/metric/metrics.go
@@ -1,18 +1,26 @@
 package metric
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
 
 var (
-	MethodDuration = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+	MethodDuration = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Name:        "broker_call_duration",
 		Help:        "calculating the latency of grpc calls",
+		Objectives: map[float64]float64{
+			0.5:  0.05,
+			0.9:  0.01,
+			0.99: 0.001,
+		},
 	},[]string{"method"})
 
-	ActiveSubscribers = prometheus.NewGauge(prometheus.GaugeOpts{
+	ActiveSubscribers = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "broker_active_subscribers",
 		Help: "number of active subscribers in broker",
 	})
-	MethodCalls = prometheus.NewCounterVec(prometheus.CounterOpts{
+	MethodCalls = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "broker_method_calls",
 		Help: "number of method calls in broker",
 	},[]string{"method"})
