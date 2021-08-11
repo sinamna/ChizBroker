8e4a106855f3f63e3c54850046a3fa5170d8e51d
sinamna <sina3050@gmail.com>
2021-08-12 03:35:43 +0430

    added auto incremented Id to subscriber

--- internal/broker/subscriber.go
+++ internal/broker/subscriber.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"therealbroker/pkg/broker"
 )
+var subscriberId = AutoIncId{id: 1}
 
 type Subscriber struct {
+	Id int
 	Channel chan broker.Message
 	Ctx     context.Context
 	unSubSignal chan *Subscriber
@@ -22,6 +24,7 @@ func (s *Subscriber) registerMessage(msg broker.Message){
 }
 func CreateNewSubscriber(ctx context.Context, ch chan broker.Message, unSubSignal chan *Subscriber)*Subscriber{
 	newSub:= &Subscriber{
+		Id: subscriberId.GetID(),
 		Channel: ch,
 		Ctx: ctx,
 		unSubSignal: unSubSignal,
