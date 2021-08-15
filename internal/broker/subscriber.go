c07b7dfc97dc930f99b7f5b3c258256bb190b9a4
sinamna <sina3050@gmail.com>
2021-08-15 16:17:50 +0430

    changed design a bit

--- internal/broker/subscriber.go
+++ internal/broker/subscriber.go
@@ -2,34 +2,43 @@ package broker
 
 import (
 	"context"
+	"sync"
 	"therealbroker/pkg/broker"
 )
 
 var subscriberId = AutoIncId{id: 1}
 
 type Subscriber struct {
+	sync.Mutex
 	Id              int
 	Channel         chan broker.Message
 	Ctx             context.Context
 	unSubSignal     chan *Subscriber
+	RegisterChannel chan *broker.Message
+
 }
 
-func (s *Subscriber) SendMessages(msg broker.Message) {
-	select {
-	case <-s.Ctx.Done():
-		go func() { s.unSubSignal <- s }()
-		return
-	default:
-		s.Channel <- msg
+func (s *Subscriber) SendMessages() {
+	for {
+		select {
+		case <-s.Ctx.Done():
+			go func() { s.unSubSignal <- s }()
+			return
+		case msg := <-s.RegisterChannel:
+			s.Channel<-*msg
+		}
 	}
 }
 func CreateNewSubscriber(ctx context.Context, ch chan broker.Message, unSubSignal chan *Subscriber) *Subscriber {
 	newSub := &Subscriber{
-		Id:          subscriberId.GetID(),
-		Channel:     ch,
-		Ctx:         ctx,
-		unSubSignal: unSubSignal,
+		Id:              subscriberId.GetID(),
+		Channel:         ch,
+		Ctx:             ctx,
+		unSubSignal:     unSubSignal,
+
+		RegisterChannel: make(chan *broker.Message),
 
 	}
+	go newSub.SendMessages()
 	return newSub
 }
