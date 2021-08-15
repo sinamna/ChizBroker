70a86ed889613239ea95c66800d47123fc098341
sinamna <sina3050@gmail.com>
2021-08-15 16:13:37 +0430

    added database interface

--- pkg/repository/database.go
+++ pkg/repository/database.go
@@ -3,7 +3,7 @@ package repository
 import "therealbroker/pkg/broker"
 
 type Database interface{
-	SaveMessage(id int, msg broker.Message)error
+	SaveMessage(id int, msg broker.Message)
 	FetchMessage(id int) (broker.Message, error)
-	DeleteMessage(id int) error
+	DeleteMessage(id int)
 }
