0ab3383b230c961e59a5785d2255a61f1feca38e
sinamna <sina3050@gmail.com>
2021-08-15 16:16:55 +0430

    done base of postgres db

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -11,13 +11,12 @@ var DB PostgresDB
 type PostgresDB struct {
 	client *sql.DB
 }
-func (db *PostgresDB) SaveMessage(id int, msg broker.Message)error{
-	return nil
+func (db *PostgresDB) SaveMessage(id int, msg broker.Message){
 }
 func (db *PostgresDB) FetchMessage(id int)(broker.Message,error){
 	return broker.Message{}, nil
 }
-func (db *PostgresDB) DeleteMessage(id int)error{
+func (db *PostgresDB) DeleteMessage(id int){
 
 }
 
