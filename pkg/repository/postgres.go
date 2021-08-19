724521bada2c40c185cb1f1f632edc9878def3c4
sinamna <sina3050@gmail.com>
2021-08-19 18:20:07 +0430

    fixed bug and added table adding

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -17,66 +17,94 @@ var connectionError error
 type PostgresDatabase struct {
 	client *sql.DB
 }
-func (db *PostgresDatabase) SaveMessage(id int, msg broker.Message, subject string)error{
+
+func (db *PostgresDatabase) SaveMessage(id int, msg broker.Message, subject string) error {
 	fmt.Println("saving")
-	rows,err := db.client.Query("call save_message($1,$2,$3,$4);", id, msg.Body, subject, int32(msg.Expiration))
-	defer rows.Close()
-	if err!= nil{
+	//rows,err := db.client.Query("call save_message($1,$2,$3,$4);", id, msg.Body, subject, int32(msg.Expiration))
+	query := fmt.Sprintf(`INSERT INTO messages(id, subject, body, expiration_date)VALUES (%d, '%s', '%s', %v);`,
+		id, msg.Body, subject, int32(msg.Expiration))
+	//fmt.Println(query)
+	rows, err := db.client.Query(query)
+	if err != nil {
 		return err
 	}
+	rows.Close()
 	return nil
 }
-func (db *PostgresDatabase) FetchMessage(id int, subject string)(broker.Message,error){
+func (db *PostgresDatabase) FetchMessage(id int, subject string) (broker.Message, error) {
 	query := fmt.Sprintf("SELECT body, expiration_date from messages where messages.id=%d and messages.subject=%s;",
-		id,subject)
+		id, subject)
 	rows, err := db.client.Query(query)
-	defer rows.Close()
-	if err!= nil{
+
+	if err != nil {
 		return broker.Message{}, err
-	}else{
-		var body string
-		var expirationDate int32
-		err := rows.Scan(&body, &expirationDate)
-		if err != nil {
-			return broker.Message{}, err
-		}
-		msg := broker.Message{
-			Body: body,
-			Expiration: time.Duration(expirationDate),
-		}
-		return msg, nil
 	}
+	var body string
+	var expirationDate int32
+	err = rows.Scan(&body, &expirationDate)
+	if err != nil {
+		return broker.Message{}, err
+	}
+	msg := broker.Message{
+		Body:       body,
+		Expiration: time.Duration(expirationDate),
+	}
+	rows.Close()
+	return msg, nil
+
 }
-func (db *PostgresDatabase) DeleteMessage(id int,subject string)error{
-	rows, err:= db.client.Query("call delete_message($1,$2);",id,subject)
-	defer rows.Close()
-	if err!=nil{
+func (db *PostgresDatabase) DeleteMessage(id int, subject string) error {
+	query := fmt.Sprintf(`DELETE FROM messages WHERE messages.id=%d and messages.subject='%s';`, id, subject)
+	rows, err := db.client.Query(query)
+
+	if err != nil {
 		return err
 	}
+	rows.Close()
 	return nil
 }
 
-func GetPostgreDB()(Database,error){
+func GetPostgreDB() (Database, error) {
 	var once sync.Once
 	once.Do(func() {
-		fmt.Println("new DB connection")
 		connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			os.Getenv("host"),os.Getenv("port"), os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
+			os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("USER"), os.Getenv("PASSWORD"), os.Getenv("DB"))
 		//fmt.Println(connString)
-		client, err:= sql.Open("postgres",connString)
-		if err!=nil{
-			connectionError=err
+		client, err := sql.Open("postgres", connString)
+		if err != nil {
+			connectionError = err
 			return
 		}
 		//defer client.Close()
 		err = client.Ping()
 		if err != nil {
-			connectionError=err
+			connectionError = err
 			return
 		}
 		fmt.Println("connected to postgres.")
+		err = createTable(client)
+		if err != nil {
+			connectionError = err
+			return
+		}
 		postgresDB = &PostgresDatabase{client: client}
 	})
-	return postgresDB,connectionError
+	return postgresDB, connectionError
 }
 
+func createTable(client *sql.DB) error {
+	table := `
+	CREATE TABLE IF NOT EXISTS messages (
+		id integer not null,
+		subject varchar(255) not null,
+		body varchar(255) not null,
+		expiration_date bigint not null,
+		primary key(id)
+);
+`
+	_, err := client.Exec(table)
+	if err != nil {
+		return err
+	}
+	return nil
+}
