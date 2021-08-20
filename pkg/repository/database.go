2d2c623f68998e08f9ecc5a3dbbe7606f290bf48
sinamna <sina3050@gmail.com>
2021-08-20 21:22:25 +0430

    removed returned err

--- pkg/repository/database.go
+++ pkg/repository/database.go
@@ -3,7 +3,7 @@ package repository
 import "therealbroker/pkg/broker"
 
 type Database interface{
-	SaveMessage(id int, msg broker.Message, subject string)error
+	SaveMessage(id int, msg broker.Message, subject string)
 	FetchMessage(id int, subject string) (broker.Message, error)
-	DeleteMessage(id int, subject string)error
+	DeleteMessage(id int, subject string)
 }
